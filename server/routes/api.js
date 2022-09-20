9979ffe6c28bc97dd07e4a4108b62f29b4e88dcd
Stan <skukhniy@gmail.com>
2022-09-20 18:04:15 -0400

    added frontend, seperated into folders

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/apiController");
-const Post = require("../models/posts");
 
 // get all posts
 router.get("/", controller.getPosts);
