466681207d5f20c2b7d8a1a66004c4c3c45c8d16
Stan <skukhniy@gmail.com>
2022-09-20 20:35:12 -0400

    added func to fetch restful API

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Admin from "./components/Admin";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [posts, setPosts] = useState(null);
+
+	// sets state and grabs post API
+	// will only fire one time
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const response = await fetch("/api/");
+			const json = await response.json();
+
+			if (response.ok) {
+				setPosts(json);
+			}
+		};
+		fetchPosts();
+	}, []);
+
+	return (
+		<div>
+			<BrowserRouter>
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route exact path="/admin/" element={<Admin />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
