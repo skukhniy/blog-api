4c896f21c9e70ba462821bc2a6ef6fe5825c73a1
Stan <skukhniy@gmail.com>
2022-09-25 22:37:15 -0400

    added props for edit post page

--- client/src/pages/AdminEditPost.js
+++ client/src/pages/AdminEditPost.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 export default function AdminEditPost({ post }) {
+	const [title, setTitle] = useState(null);
+	const [content, setContent] = useState(null);
+	const [topic, setTopic] = useState(null);
+
 	return (
 		<div className="">
 			<h4 className="text-center">Post ID: {post._id}</h4>
@@ -14,7 +18,10 @@ export default function AdminEditPost({ post }) {
 						<label className="">Title:</label>
 					</Col>
 					<Col>
-						<input value={post.title}></input>
+						<input
+							value={post.title}
+							onChange={(e) => setTitle(e.target.value)}
+						></input>
 					</Col>
 					<Col xs={4}></Col>
 				</Row>
@@ -24,7 +31,12 @@ export default function AdminEditPost({ post }) {
 						<label className="">Content:</label>
 					</Col>
 					<Col>
-						<textarea value={post.content}>{post.content}</textarea>
+						<textarea
+							value={post.content}
+							onChange={(e) => setContent(e.target.value)}
+						>
+							{post.content}{" "}
+						</textarea>
 					</Col>
 					<Col xs={4}></Col>
 				</Row>
@@ -34,17 +46,11 @@ export default function AdminEditPost({ post }) {
 						<label>Topic:</label>
 					</Col>
 					<Col>
-						<input className="" value={post.topic}></input>
-					</Col>
-					<Col xs={4}></Col>
-				</Row>
-				<Row>
-					<Col xs={5}></Col>
-					<Col>
-						<label>Topic:</label>
-					</Col>
-					<Col>
-						<input className="" value={post.topic}></input>
+						<input
+							className=""
+							value={post.topic}
+							onChange={(e) => setTopic(e.target.value)}
+						></input>
 					</Col>
 					<Col xs={4}></Col>
 				</Row>
