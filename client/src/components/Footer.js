46794e9efe4a8c52701f065b093bdc822dc0de25
Stan <skukhniy@gmail.com>
2022-12-21 11:24:01 -0500

    added relevant social media/links to the footer, installed social Icon react package, adjusted padding

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,21 +1,42 @@
 import React from 'react';
+import { SocialIcon } from 'react-social-icons';
 
 export default function Footer() {
   return (
     <div className="footer">
       <div>
-        <h3>Where to Find Me</h3>
+        <h3></h3>
       </div>
-      <div></div>
-      <div className="github">
-        <a
-          href="https://github.com/skukhniy"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          &copy; https://github.com/skukhniy
-        </a>
+      <div>
+        <SocialIcon
+          url="skukhniy@gmail.com"
+          network="email"
+          bgColor="transparent"
+          fgColor="rgb(127, 127, 127)"
+        />
+        <SocialIcon
+          url="https://www.linkedin.com/in/stanislav-kukhniy-5b12a1138/"
+          fgColor="rgb(0, 127, 177)"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.instagram.com/skukhniy/"
+          fgColor="rgb(233, 68, 117)"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://twitter.com/SKukhniy"
+          fgColor="rgb(0, 172, 237)"
+          bgColor="transparent"
+        />
+
+        <SocialIcon
+          url="https://github.com/skukhniy"
+          fgColor="rgb(36, 41, 46)"
+          bgColor="transparent"
+        />
       </div>
+      <div className="github">&copy; Stanislav Kukhniy</div>
     </div>
   );
 }
--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,19 +1,19 @@
-import React from "react";
-import parse from "html-react-parser";
+import React from 'react';
+import parse from 'html-react-parser';
 
 export default function PostPage({ post }) {
-	const date = new Date(post.date);
-	return (
-		<div className="d-flex justify-content-center postPage">
-			<div
-				className="pt-4 pe-3 ps-3 d-flex flex-column w-75"
-				style={{ backgroundColor: "white", height: "100%" }}
-			>
-				<h1 className="mb-2">{post.title}</h1>
-				<p className="mb-4">{`Posted on ${date.toDateString()}`}</p>
-				<img src={post.img}></img>
-				<div className="mt-4 pe-2 ps-2">{parse(post.content)}</div>
-			</div>
-		</div>
-	);
+  const date = new Date(post.date);
+  return (
+    <div className="d-flex justify-content-center postPage">
+      <div
+        className="pt-4 pe-3 ps-3 d-flex flex-column w-75 displayContent"
+        style={{ backgroundColor: 'white', height: '100%' }}
+      >
+        <h1 className="mb-2">{post.title}</h1>
+        <p className="mb-4">{`Posted on ${date.toDateString()}`}</p>
+        <img src={post.img}></img>
+        <div className="mt-4 pe-2 ps-2">{parse(post.content)}</div>
+      </div>
+    </div>
+  );
 }
