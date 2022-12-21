04b319fe117789a806248433d91536f6cc2ab366
Stan <skukhniy@gmail.com>
2022-12-21 11:37:42 -0500

    fixed the preview text so it adds a colon and a space before a starting header, adjusted sliced string amount

--- client/src/components/BlogCards.js
+++ client/src/components/BlogCards.js
@@ -1,38 +1,39 @@
-import "../styles/App.scss";
-import React from "react";
-import { Link } from "react-router-dom";
+import '../styles/App.scss';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function BlogCards({ post, adminCheck }) {
-	// format date
-	let date = new Date(post.date);
-	date = date.toLocaleDateString("en-us", {
-		year: "numeric",
-		month: "short",
-		day: "numeric",
-	});
+  // format date
+  let date = new Date(post.date);
+  date = date.toLocaleDateString('en-us', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  });
 
-	// format preview text
-	let content = post.content;
-	content = content.replace(/<\/?[^>]+(>|$)/g, "");
+  // format preview text
+  let content = post.content;
+  content = content.replace(/<\/h1>/g, ': ');
+  content = content.replace(/<\/?[^>]+(>|$)/g, '');
 
-	let postLink;
-	if (adminCheck) {
-		postLink = `/administrator/post/${post._id}`;
-		content = content.split(" ").slice(0, 10).join(" ") + "...";
-	} else {
-		postLink = `/post/${post._id}`;
-		content = content.split(" ").slice(0, 15).join(" ") + "...";
-	}
+  let postLink;
+  if (adminCheck) {
+    postLink = `/administrator/post/${post._id}`;
+    content = content.split(' ').slice(0, 10).join(' ') + '...';
+  } else {
+    postLink = `/post/${post._id}`;
+    content = content.split(' ').slice(0, 20).join(' ') + '...';
+  }
 
-	return (
-		<div className={`border me-1 ms-1 p-2 mt-4 shadow blogCard`}>
-			<Link className="text-decoration-none text-dark" to={postLink}>
-				<img src={post.img}></img>
-				<h3>{post.title}</h3>
-				<p>{date}</p>
-				<p>{content}</p>
-			</Link>
-			{/* <p>{`${post.content.substring(0, 50)}...`}</p> */}
-		</div>
-	);
+  return (
+    <div className={`border me-1 ms-1 p-2 mt-4 shadow blogCard`}>
+      <Link className="text-decoration-none text-dark" to={postLink}>
+        <img src={post.img}></img>
+        <h3>{post.title}</h3>
+        <p>{date}</p>
+        <p>{content}</p>
+      </Link>
+      {/* <p>{`${post.content.substring(0, 50)}...`}</p> */}
+    </div>
+  );
 }
