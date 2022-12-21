07574b7f472db1eb79a1a0457805c31f18ebc3f8
Stan <skukhniy@gmail.com>
2022-12-20 22:40:48 -0500

    added new divs to footer, added github copyright, and adjusted margins for the home page

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,7 +4,17 @@ export default function Footer() {
   return (
     <div className="footer">
       <div>
-        <h1>Footer</h1>
+        <h3>Where to Find Me</h3>
+      </div>
+      <div></div>
+      <div className="github">
+        <a
+          href="https://github.com/skukhniy"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          &copy; https://github.com/skukhniy
+        </a>
       </div>
     </div>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,51 +1,51 @@
-import React from "react";
-import BlogCards from "../components/BlogCards";
-import me from "../assets/imgs/Me.png";
+import React from 'react';
+import BlogCards from '../components/BlogCards';
+import me from '../assets/imgs/Me.png';
 
 export default function Home({ posts }) {
-	// generate cards for each post
-	// const renderCards = posts.map((post) => <BlogCards post={post} />);
-	console.log(posts);
-	let filteredPosts = posts;
-	// if not null, return a filtered array of posts
-	if (filteredPosts !== null) {
-		// sort by published === true
-		filteredPosts = filteredPosts.filter((post) => post.published);
-		// sort by newest date to oldest date
-		filteredPosts = filteredPosts.sort((a, b) => {
-			var dateA = new Date(a.date);
-			var dateB = new Date(b.date);
-			return dateB - dateA;
-		});
-		console.log(filteredPosts);
-	}
-	return (
-		<div className="">
-			<div className="homeHeader">
-				<div className="innerDiv d-sm-flex-col text-center">
-					<img
-						className="rounded-circle headerPic img-fluid"
-						src={me}
-						alt="Its me!"
-					></img>
-					<h2 className="text-center mt-0 mb-0 font-weight-bolder">
-						Stanislav Kukhniy
-					</h2>
-					<p className="text-center mt-1">
-						Welcome to my blog! This is where I track my progress in learning
-						web development, and software engineering in general.
-					</p>
-				</div>
-			</div>
-			<div
-				className="d-flex justify-content-around flex-wrap text-center"
-				style={{ "margin-left": "5%", "margin-right": "5%" }}
-			>
-				{filteredPosts &&
-					filteredPosts.map((post) => (
-						<BlogCards post={post} adminCheck={false} />
-					))}
-			</div>
-		</div>
-	);
+  // generate cards for each post
+  // const renderCards = posts.map((post) => <BlogCards post={post} />);
+  console.log(posts);
+  let filteredPosts = posts;
+  // if not null, return a filtered array of posts
+  if (filteredPosts !== null) {
+    // sort by published === true
+    filteredPosts = filteredPosts.filter((post) => post.published);
+    // sort by newest date to oldest date
+    filteredPosts = filteredPosts.sort((a, b) => {
+      var dateA = new Date(a.date);
+      var dateB = new Date(b.date);
+      return dateB - dateA;
+    });
+    console.log(filteredPosts);
+  }
+  return (
+    <div className="home">
+      <div className="homeHeader">
+        <div className="innerDiv d-sm-flex-col text-center">
+          <img
+            className="rounded-circle headerPic img-fluid"
+            src={me}
+            alt="Its me!"
+          ></img>
+          <h2 className="text-center mt-0 mb-0 font-weight-bolder">
+            Stanislav Kukhniy
+          </h2>
+          <p className="text-center mt-1">
+            Welcome to my blog! This is where I track my progress in learning
+            web development, and software engineering in general.
+          </p>
+        </div>
+      </div>
+      <div
+        className="d-flex justify-content-around flex-wrap text-center"
+        style={{ 'margin-left': '5%', 'margin-right': '5%' }}
+      >
+        {filteredPosts &&
+          filteredPosts.map((post) => (
+            <BlogCards post={post} adminCheck={false} />
+          ))}
+      </div>
+    </div>
+  );
 }
